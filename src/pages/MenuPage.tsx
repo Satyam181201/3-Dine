6de4a3aae1f614a6ce32c416c799dae5a0b0d649
Satyam Nayan Shah <103631166+Satyam181201@users.noreply.github.com>
2024-02-09 15:44:42 +0530

    menu page

--- src/pages/MenuPage.tsx
+++ src/pages/MenuPage.tsx
@@ -7,7 +7,7 @@ import MenuItem from '../components/menu/MenuItem'
 
 import { Link } from 'react-router-dom'
 // Import your background image
-import backgroundImage from '../assets/homepage/menu-bg-tablet.jpg';
+import backgroundImage from '../assets/homepage/menu-bg-tablet@2x.jpg';
 
 const MunuPage: FC = () => {
    return (
